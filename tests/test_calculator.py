ce0cace9d263e5bc5b57f7b86b03261d8ebc0a44
Chris Abdo <chris.j.abdo@gmail.com>
2024-09-27 08:44:58 -0400

    part 2

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,11 +1,18 @@
-'''My Calculator Test'''
-from calculator import add, subtract
+"""Tests for the Calculator class."""
+from calculator import Calculator
 
 def test_addition():
-    '''Test that addition function works '''    
-    assert add(2,2) == 4
+    """Test that addition function works correctly."""
+    assert Calculator.add(2, 2) == 4
 
 def test_subtraction():
-    '''Test that addition function works '''    
-    assert subtract(2,2) == 0
-    
+    """Test that subtraction function works correctly."""
+    assert Calculator.subtract(2, 2) == 0
+
+def test_multiply():
+    """Test that multiplication function works correctly."""
+    assert Calculator.multiply(2, 2) == 4
+
+def test_divide():
+    """Test that division function works correctly."""
+    assert Calculator.divide(2, 2) == 1
