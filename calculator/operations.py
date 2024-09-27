d325f7f75a4926bb36d42f6c1b7fb6e6bf576277
Chris Abdo <chris.j.abdo@gmail.com>
2024-09-27 09:11:50 -0400

    operations.py type hinting and divide by 0 exception

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,11 +1,18 @@
-def add(a,b):
+from decimal import Decimal
+
+
+# type hinting
+def add(a: Decimal, b: Decimal) -> Decimal:
     return a + b
 
-def subtract(a,b):
+def subtract(a: Decimal, b: Decimal) -> Decimal:
     return a - b
 
-def multiply(a,b):
+def multiply(a: Decimal, b: Decimal) -> Decimal:
     return a * b
 
-def divide(a,b):
+def divide(a: Decimal, b: Decimal) -> Decimal:
+    # division by zero exception
+    if b == 0:
+        raise Exception("Cannot divide by zero!")
     return a / b
